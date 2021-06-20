992dcc634a92e7f7238ac6b6194b336cd7297575
wlfoj <wolivej@gmail.com>
2021-06-19 21:07:03 -0300

    added pathToDataset

--- preprocessing data/main.py
+++ preprocessing data/main.py
@@ -3,9 +3,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+pathToDataset = 'images/Images'
+
 ## Módulo para criar o target name
-def createTargetName():
-    pastas = os.listdir('images/Images')
+def createTargetName(pathToDataset):
+    pastas = os.listdir(pathToDataset)
     pastas.sort() # Para padronizar
     racas = []
     for i in range(len(pastas)):
@@ -25,14 +27,14 @@ def createTargetName():
 
 ## Módulo para criar target (númerico)
 # Com esta função, cada imagem terá um número como rótulo
-def createTarget():
-    pastas = os.listdir('images/Images')
+def createTarget(pathToDataset):
+    pastas = os.listdir(pathToDataset)
     pastas.sort() # Para padronizar
     racas = []
     alvo = 0
     #
     for i in range(len(pastas)):
-        arquivos = os.listdir('images/Images/'+pastas[i])
+        arquivos = os.listdir(pathToDataset+'/'+pastas[i])
         for i in range (0, len(arquivos)):
             racas.append(alvo)
         alvo = alvo + 1
@@ -50,13 +52,13 @@ def createTarget():
 
 ## Módulo para criar target (nome da raça)
 # Com esta função, cada imagem terá o nome da raça como rótulo
-def targetTESTE():
-    pastas = os.listdir('images/Images')
+def targetTESTE(pathToDataset):
+    pastas = os.listdir(pathToDataset)
     pastas.sort() # Para padronizar
     racas = []
     # 
     for i in range(len(pastas)):
-        arquivos = os.listdir('images/Images/'+pastas[i])
+        arquivos = os.listdir(pathToDataset+'/'+pastas[i])
         nomeracas = pastas[i].split('-')
         for i in range (0, len(arquivos)):
             racas.append(nomeracas[1])
@@ -73,7 +75,7 @@ def targetTESTE():
 
 
 ## Módulo para ler o target (númerico)
-def loadTarget():
+def loadTarget(pathToDataset):
     target = []
     with open('target/target.txt','r') as arquivo:
         for linha in arquivo:
@@ -83,7 +85,7 @@ def loadTarget():
 
 ## Módulo para ler o target name
 # Função para obter a raça correspondente ao alvo númerico
-def loadTargetNames():
+def loadTargetNames(pathToDataset):
     targetName = []
     with open('target/targetNames.txt','r') as arquivo:
         for linha in arquivo:
@@ -92,13 +94,13 @@ def loadTargetNames():
 
 
 ## Módulo para retornar as fotos no formato indicado TensorFlow
-def loadFeatures():
-    pastas = os.listdir('images/Images')
+def loadFeatures(pathToDataset):
+    pastas = os.listdir(pathToDataset)
     pastas.sort() # Para padronizar
     features = []
     for i in range(len(pastas)): # Acessando as pasta
-        arquivos = os.listdir('images/Images/'+pastas[i])
+        arquivos = os.listdir(pathToDataset+'/'+pastas[i])
         for j in range (0, len(arquivos)): # Acessando os arquivos
-            image = cv2.imread('images/Images/'+pastas[i]+'/'+arquivos[j]) # Transformando arquivo de foto em matriz numpy
+            image = cv2.imread(pathToDataset+'/'+pastas[i]+'/'+arquivos[j]) # Transformando arquivo de foto em matriz numpy
             features.append(image)
     return np.array(features)
